76e10a2bb6a97ff1488a46324edea648b30ea983
stefi.laber <stefi.laber@yahoo.com>
2023-11-24 19:07:12 +0100

    added optional and changed to custom files

--- main.cpp
+++ main.cpp
@@ -7,18 +7,28 @@
 #include <iterator>
 #include <ranges>
 #include <map>
+#include <optional>
 
-auto readFile = [](const std::string& filename) -> std::vector<std::string> {
+auto readFile = [](const std::string& filename) -> std::optional<std::vector<std::string>> {
     std::ifstream file(filename);
     if (!file.is_open()) {
         throw std::runtime_error("Could not open file");
     }
+
+    // chek if the file is empty
+    if (file.peek() == std::ifstream::traits_type::eof()) {
+        return std::nullopt;
+    }
+
     std::vector<std::string> lines;
     std::string line;
     while (std::getline(file, line)) {
+        if(line.find("END OF THE PROJECT GUTENBERG EBOOK, WAR AND PEACE") != std::string::npos){
+            return lines;
+        }
         lines.push_back(line);
     }
-    return lines;
+    return make_optional(lines);
 };
 
 auto tokenizeText = [](const std::string& text) -> std::vector<std::string> {
@@ -118,32 +128,51 @@ auto calculateTermDensity = [](const std::vector<std::string>& words, const std:
 };
 
 int main() {
-    std::vector<std::string> bookLines = readFile("book.in");
-    std::vector<std::string> warTermsLines = readFile("war_terms.in");
-    std::vector<std::string> peaceTermsLines = readFile("peace_terms.in");
+    std::string filename;
+
+    std::cout << " enter the name of the book file: ";
+    std::cin >> filename;
+    std::optional<std::vector<std::string>> bookLines = readFile(filename);
+    if (bookLines == std::nullopt) {
+        std::cout << "The file is empty\n";
+        return 0;
+    }
+
+    std::cout << " enter the name of the war terms file: ";
+    std::cin >> filename;
+    std::optional<std::vector<std::string>> warTermsLines = readFile(filename);
+    if (warTermsLines == std::nullopt) {
+        std::cout << "The file is empty\n";
+        return 0;
+    }
+
+    std::cout << " enter the name of the war terms file: ";
+    std::cin >> filename;
+    std::optional<std::vector<std::string>> peaceTermsLines = readFile(filename);
+    if (peaceTermsLines == std::nullopt) {
+        std::cout << "The file is empty\n";
+        return 0;
+    }
 
     std::vector<std::string> bookWords;
-    for (const auto& line : bookLines) {
+    for (const auto& line : bookLines.value()) {
         std::vector<std::string> tokens = tokenizeText(line);
         bookWords.insert(bookWords.end(), tokens.begin(), tokens.end());
     }
 
     std::vector<std::string> warTermsWords;
-    for (const auto& line : warTermsLines) {
+    for (const auto& line : warTermsLines.value()) {
         std::vector<std::string> tokens = tokenizeText(line);
         warTermsWords.insert(warTermsWords.end(), tokens.begin(), tokens.end());
     }
 
     std::vector<std::string> peaceTermsWords;
-    for (const auto& line : peaceTermsLines) {
+    for (const auto& line : peaceTermsLines.value()) {
         std::vector<std::string> tokens = tokenizeText(line);
         peaceTermsWords.insert(peaceTermsWords.end(), tokens.begin(), tokens.end());
     }
 
     std::vector<std::vector<std::string>> chapters = splitIntoChapters(bookWords);
-
-    // std::vector<double> peaceDensities;
-    // std::vector<double> warDensities;
 
     int chapterNumber = 1;
 
